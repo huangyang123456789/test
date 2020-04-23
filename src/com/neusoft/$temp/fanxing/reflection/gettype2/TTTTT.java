46a5ff2ea4bbec67d129dc54173990d095c9941e
kong-1999 <1999kong>
2020-04-23 15:57:49 +0800

    测试

---
+++ src/com/neusoft/$temp/fanxing/reflection/gettype2/TTTTT.java
@@ -0,0 +1,11 @@
+package com.neusoft.$temp.fanxing.reflection.gettype2;
+
+/**
+ * @Author: xiaoming
+ * @Date: 2020/4/23 0023 15:50
+ */
+public class TTTTT {
+    public static void main(String[] args) {
+        System.out.println("FBI!open the door!");
+    }
+}
