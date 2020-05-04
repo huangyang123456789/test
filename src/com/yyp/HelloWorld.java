39815303d95db309ec5c4ffd7a7375bbb9e101bf
hp-yYp <1092916593@qq.com>
2020-05-04 22:44:07 +0800

    测试Git

---
+++ src/com/yyp/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.yyp;
+
+/**
+ * 你好世界
+ *
+ * @Author: wwb
+ * @Date: 2020/5/4 22:36
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
